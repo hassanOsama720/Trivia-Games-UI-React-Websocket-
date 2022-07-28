22cecb855131a0ca529395d9550c05ee91aab899
hassanOsama720 <hassan.osama720@gmail.com>
2022-07-28 19:44:10 +0200

    update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function Landing() {
     const nav = useNavigate()
   return (
-    <div className="container-fluid p-0 vh-100 w-100 d-flex flex-column align-items-center justify-content-evenly"
+    <div className="container p-0 vh-100 w-100 d-flex flex-column align-items-center justify-content-evenly"
      style={{backgroundImage: `url("/Welcome.jpg")`,backgroundSize:"cover"}} onClick={()=>{nav("/manage")}}>
     </div>    
   )
